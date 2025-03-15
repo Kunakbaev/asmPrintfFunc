8a91fa30fdd249ef682ccfe57b83d1b8c55688be
Kunakbaev <tyrep2017@mail.ru>
2025-03-15 19:51:10 +0300

    added string type format output

--- asmPrintf.asm
+++ asmPrintf.asm
@@ -4,30 +4,34 @@ extern printf
 extern atexit
 
 section .data
-    MAX_FORMAT_STR_BUFF_LEN     equ 100
-    MAX_TMP_BUFF_LEN            equ 100
-    DECIMAL_FORMAT_STRING       equ 'd'
-    BOOL_FORMAT_STRING          equ 'b'
-    OCTAL_FORMAT_STRING         equ 'o'
-    HEX_FORMAT_STRING           equ 'x'
-    CHAR_FORMAT_STRING          equ 'c'
-    STRING_FORMAT_STRING        equ 's'
-    FORMAT_STRING_DELIM         equ '%'
-    ERROR_EXIT_CODE             equ 228
-    TRUE_STRING_LEN             equ 4
-    FALSE_STRING_LEN            equ 5
-    MAX_OUTPUT_BUFFER_LEN       equ 10
+    MAX_FORMAT_STR_BUFF_LEN          equ 100
+    MAX_TMP_BUFF_LEN                 equ 100
+    DECIMAL_FORMAT_STRING            equ 'd'
+    BOOL_FORMAT_STRING               equ 'b'
+    OCTAL_FORMAT_STRING              equ 'o'
+    HEX_FORMAT_STRING                equ 'x'
+    CHAR_FORMAT_STRING               equ 'c'
+    STRING_FORMAT_STRING             equ 's'
+    FORMAT_STRING_DELIM              equ '%'
+    ERROR_EXIT_CODE                  equ 228
+    TRUE_STRING_LEN                  equ 4
+    FALSE_STRING_LEN                 equ 5
+    MAX_OUTPUT_BUFFER_LEN            equ 10
+    MAX_ARG_STRING_LEN               equ 100
+    SYSCALL_EXIT_FUNC_IND            equ 0x3C
+    SYSCALL_STANDART_OUTPUT_FUNC_IND equ 0x01
 
-    tmpStringBuff               db MAX_TMP_BUFF_LEN dup(0)
-    formatStringBuff            db MAX_FORMAT_STR_BUFF_LEN dup(0)
-    errorMessage                db "Some error has occured(", 10
-    errorMessageLen             equ 24
-    newLine                     db 10
-    trueString                  db "true"
-    falseString                 db "false"
 
-    outputBufferString          db MAX_OUTPUT_BUFFER_LEN dup(0)
-    numOfCharsInOutputBuffer    dd 0
+    tmpStringBuff                    db MAX_TMP_BUFF_LEN dup(0)
+    formatStringBuff                 db MAX_FORMAT_STR_BUFF_LEN dup(0)
+    ; errorMessage                     db "Some error has occured(", 10
+    ; errorMessageLen                  equ 24
+    newLine                          db 10
+    trueString                       db "true", 0
+    falseString                      db "false", 0
+
+    outputBufferString               db MAX_OUTPUT_BUFFER_LEN dup(0)
+    numOfCharsInOutputBuffer         dd 0 ; double word
 
 
 
@@ -37,11 +41,18 @@ section .data
 section .text
 
 exitProgrammWithError:
-    mov eax, 0x3C
+    mov eax, SYSCALL_EXIT_FUNC_IND
     mov ebx, ERROR_EXIT_CODE
 
     syscall
     ret ; ???
+
+; entry: RSI - address of entry string
+printGivenString:
+    mov rax, SYSCALL_STANDART_OUTPUT_FUNC_IND ; syscall index of standard output function
+    mov rdi, 1    ; file descriptor for stdout
+    syscall
+    ret
 
 ; entry: RDX - string len
 ;        RSI - address of source string
@@ -51,24 +62,13 @@ clearAndOutputBuffer:
     push rdx
     push rax
 
-;     mov rax, 0x01 ; syscall index of standard output function
-;     mov rdi, 1    ; file descriptor for stdout
-;     mov rsi, trueString
-;     mov rdx, TRUE_STRING_LEN
-;
-;     syscall
-
-
-
-    mov rax, 0x01 ; syscall index of standard output function
-    mov rdi, 1    ; file descriptor for stdout
-    mov rsi, outputBufferString
-
     mov rdx, [numOfCharsInOutputBuffer] ; len of buffer
     ; numOfCharsInOutputBuffer = 0
     mov word [numOfCharsInOutputBuffer], 0
 
-    syscall
+    mov rsi, outputBufferString
+    call printGivenString
+
     pop rax
     pop rdx
     pop rsi
@@ -87,7 +87,6 @@ addChar2Buffer:
     mov ebx, [numOfCharsInOutputBuffer]
     add rdi, rbx
     stosb
-    ; inc
 
     mov eax, [numOfCharsInOutputBuffer]
     inc eax
@@ -98,7 +97,7 @@ addChar2Buffer:
         call clearAndOutputBuffer
     notYetTimeToFlush:
 
-    pop rcx ; ASK: who did change rcx?
+    pop rcx ; ASK: what did change rcx?
     pop rbx
     pop rax
     pop rdi
@@ -161,53 +160,84 @@ printNumberInSomeBase:
     pop rcx
     ret
 
-; ENTRY: al - char to print
+; entry: AL - char to print
 printSingleChar:
     call addChar2Buffer
     ret
 
-; ENTRY: rax - address of a string
+; considers that there's enough space for a string in the buffer
+; entry: RSI - string memory address
+;        RCX - string len
+addString2Buffer:
+    xor rax, rax
+    mov eax, [numOfCharsInOutputBuffer]
+    mov rdi, outputBufferString
+    add rdi, rax
+    add rax, rcx
+    mov [numOfCharsInOutputBuffer], eax
+    rep movsb
+
+    ret
+
+; entry: RAX - address of a string
 printString:
     push rsi
     push rdi
 
+    ; find string len
     push rax ; save string address
     xor rdx, rdx ; rdx = 0
     sub rdx, rax ; prepare string len reg
 
     mov rdi, rax ; load string address
-    mov rcx, 100 ; max string len
+    mov rcx, MAX_ARG_STRING_LEN ; max string len
     mov al, 0
     repne scasb ; search for terminating char
     add rdx, rdi
 
-    ; ; print string len
-    ; mov rbx, 10
-    ; mov rax, rdx
-    ; call printNumberInSomeBase
-
     pop rsi ; restore string address
-    ; call myPrint
+    mov rbx, MAX_OUTPUT_BUFFER_LEN
+    sub rbx, rcx ; calculate left space in buffer
+
+
+    ; 3 cases (based on left space in output buffer and string len):
+    ; 1) if there's enough space for a string in the buffer, we just add it to the buffer
+    ; 2) there's not enough space for a string in the buffer, but it still can fit in it,
+    ;    so first, we flush the buffer and then add string to it
+    ; 3) string is too long, to fit even in empty buffer, so we just straight ahead output it
+    cmp rbx, rcx
+    jge wholeStringInBuffer
+        cmp rcx, MAX_OUTPUT_BUFFER_LEN
+        call clearAndOutputBuffer
+        jge outputWholeStringAtOnce
+            call addString2Buffer
+            jmp outputWholeStringAtOnceIfEnd
+        outputWholeStringAtOnce:
+            call printGivenString
+        outputWholeStringAtOnceIfEnd:
+        jmp wholeStringInBufferIfEnd
+    wholeStringInBuffer:
+        ; store whole string into the buffer
+        call addString2Buffer
+    wholeStringInBufferIfEnd:
 
     pop rdi
     pop rsi
     ret
 
-; ENTRY: rax - variable
+; entry: RAX - variable
 printBoolean:
     push rsi
     push rdx
 
     cmp rax, 1
     je valIsTrue
-        mov rdx, FALSE_STRING_LEN
-        mov rsi, falseString
-        ;call myPrint
+        mov rax, falseString
+        call printString
         jmp valIsTrueIfEnd
     valIsTrue:
-        mov rdx, TRUE_STRING_LEN
-        mov rsi, trueString
-        ;call myPrint
+        mov rax, trueString
+        call printString
     valIsTrueIfEnd:
 
     pop rdx
@@ -216,18 +246,11 @@ printBoolean:
 
 ; System V calling convention (first 6 args are passed through registers and remaining are put to the stack)
 myPrintfFunction:
-    ;and rsp,-16
-    ; push rdi
-    ; ;sub rsp, 8
-    ; mov rdi, trueString
-    ; call printf
-    ; ;add rsp, 8
-    ; pop rdi
-    ; ;ret
-
     ; For now function only accepts arguments of INTEGER types (addresses, chars, ints)
     ; each argument is rounded up to 8 bytes
     ; arguments are put to registers in that order: RDI, RSI, RDX, RCX, R8, R9
+    ; if there are more arguments, they are passed through stack in RTL (right to left order)
+    ; so last argument is pushed first
     push r9
     push r8
     push rcx
@@ -235,7 +258,7 @@ myPrintfFunction:
     push rsi
     push rdi
 
-    ; calling C function atexit, to set function that will be called at the end
+    ; calling C function atexit, to set handler that will be called at the end of C programm
     ; ASK: how does it work? what if my function overwrites smth important?
     sub rsp, 8
     mov rdi, clearAndOutputBuffer
@@ -258,15 +281,8 @@ myPrintfFunctionCdeclFormat:
     mov rbp, rsp
     add rbp, 8
     mov rdi, 1
-    mov rsi, [rbp + 8 * rdi]
+    mov rsi, [rbp + 8 * rdi] ; load format string (first argument of printf)
     inc rdi ; current argument index
-
-    ; mov rax, 0x01 ; syscall index of standard output function
-    ; mov rdi, 1
-    ; syscall
-
-    ; pop rbp
-    ; ret
 
     formatStringCharsLoop:
         ; ASK: how to fix this?
@@ -346,7 +362,6 @@ myPrintfFunctionCdeclFormat:
         jmp formatStringCharsLoop
     formatStringLoopEnd:
 
-    ; TODO:
     ; call clearAndOutputBuffer
 
     pop rbp
