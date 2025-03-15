4d9361e9892b8e04bdd8ca76842f024e890203bd
Kunakbaev <tyrep2017@mail.ru>
2025-03-15 14:08:44 +0300

    added formats functions (bool, char, string)

--- asmPrintf.asm
+++ asmPrintf.asm
@@ -2,17 +2,26 @@ global myPrintfFunction
 
 section .data
     MAX_FORMAT_STR_BUFF_LEN     equ 100
-    helloMessageLen             equ 31
     MAX_TMP_BUFF_LEN            equ 100
+    DECIMAL_FORMAT_STRING       equ 'd'
+    BOOL_FORMAT_STRING          equ 'b'
+    OCTAL_FORMAT_STRING         equ 'o'
+    HEX_FORMAT_STRING           equ 'x'
+    CHAR_FORMAT_STRING          equ 'c'
+    STRING_FORMAT_STRING        equ 's'
     FORMAT_STRING_DELIM         equ '%'
     ERROR_EXIT_CODE             equ 228
 
-    helloMessage:               db "Hello C world, from asm world!", 10
     tmpStringBuff               db MAX_TMP_BUFF_LEN dup(0)
     formatStringBuff            db MAX_FORMAT_STR_BUFF_LEN dup(0)
     errorMessage                db "Some error has occured(", 10
-    errorMessageLen             db 24
+    errorMessageLen             equ 24
     newLine                     db 10
+    trueString                  db "true"
+    trueStringLen               equ 4
+    falseString                 db "false"
+    falseStringLen              equ 5
+
 
 
 
@@ -31,10 +40,12 @@ exitProgrammWithError:
 ;        RSI - address of source string
 ;
 myPrint:
-    mov rax, 0x01               ; syscall index of standard output function
+    push rdi
+    mov rax, 0x01 ; syscall index of standard output function
     mov rdi, 1    ; file descriptor for stdout
 
     syscall
+    pop rdi
     ret
 
 printNewLine:
@@ -52,18 +63,30 @@ showErrorMessage:
 
     ret
 
-; rax number
-printNumberInDecimal:
+; entry: RBX base
+;        RAX number
+printNumberInSomeBase:
+    push rcx
+    push rdx
+    push rax
+    push rdi
+
     mov rdi, tmpStringBuff
     xor rcx, rcx
 
     digitLoop:
-        mov rbx, 10
         xor rdx, rdx
         div rbx ; reminder to edx
         push rax
         mov rax, rdx
-        add rax, '0'
+
+        cmp rax, 10
+        jl reminderIsDigit
+            add rax, 'A' - 10
+            jmp reminderIsDigitIfEnd
+        reminderIsDigit:
+            add rax, '0'
+        reminderIsDigitIfEnd:
         stosb
         pop rax
 
@@ -75,83 +98,179 @@ printNumberInDecimal:
     mov rsi, tmpStringBuff
     call myPrint
 
+    pop rdi
+    pop rax
+    pop rdx
+    pop rcx
     ret
 
+; ENTRY: al - char to print
+printSingleChar:
+    push rdi
+    mov rdi, tmpStringBuff
+    stosb
+    mov rsi, tmpStringBuff
+    mov rdx, 1
+    call myPrint
+    pop rdi
+    ret
 
+; ENTRY: rax - address of a string
+printString:
+    push rsi
+    push rdi
+
+    push rax ; save string address
+    xor rdx, rdx ; rdx = 0
+    sub rdx, rax ; prepare string len reg
+
+    mov rdi, rax ; load string address
+    mov rcx, 100 ; max string len
+    mov al, 0
+    repne scasb ; search for terminating char
+    add rdx, rdi
+
+    ; ; print string len
+    ; mov rbx, 10
+    ; mov rax, rdx
+    ; call printNumberInSomeBase
+
+    pop rsi ; restore string address
+    call myPrint
+
+    pop rdi
+    pop rsi
+    ret
+
+; ENTRY: rax - variable
+printBoolean:
+    push rsi
+    push rdx
+
+    cmp rax, 1
+    je valIsTrue
+        mov rdx, falseStringLen
+        mov rsi, falseString
+        call myPrint
+        jmp valIsTrueIfEnd
+    valIsTrue:
+        mov rdx, trueStringLen
+        mov rsi, trueString
+        call myPrint
+    valIsTrueIfEnd:
+
+    pop rdx
+    pop rsi
+    ret
+
+; System V calling convention (first 6 args are passed through registers and remaining are put to the stack)
 myPrintfFunction:
-;     push rbp
-;     mov rbp, rsp
-; ;
-    ; mov rdi, [rbp-8]
-    ; mov rsi, [rbp-16]
-    ; push rsi
+    ; For now function only accepts arguments of INTEGER types (addresses, chars, ints)
+    ; each argument is rounded up to 8 bytes
+    ; arguments are put to registers in that order: RDI, RSI, RDX, RCX, R8, R9
+    push r9
+    push r8
+    push rcx
+    push rdx
+    push rsi
+    push rdi
 
-    ; mov rdx, helloMessageLen
-    ; mov rcx, rdx
-    ; mov rsi, helloMessage
-    ; mov rdi, tmpStringBuff
-    ; rep movsb
-    ; call myPrint
+    call myPrintfFunctionCdeclFormat
 
-    ; mov rax, 22891
-    ; mov rdx, dword edi
-    ; mov rax, rdi
-    ; call printNumberInDecimal
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop r8
+    pop r9
 
-;     push rsi
-;     mov rax, rdi
-;     call printNumberInDecimal
-;
-;     mov rcx, 10
-;     pop rsi
-;     mov rdi, tmpStringBuff
-;     rep movsb
-;     mov rdx, 10
-;     call myPrint
+    ret
 
+myPrintfFunctionCdeclFormat:
+    push rbp
+    mov rbp, rsp
+    add rbp, 8
+    mov rdi, 1
+    mov rsi, [rbp + 8 * rdi]
+    inc rdi ; current argument index
 
-
-    mov rsi, rdi
     formatStringCharsLoop:
+        ; ASK: how to fix this?
+        cmp rdi, 7
+        jne notArgumentGap ; skip argument gap
+            inc rdi
+        notArgumentGap:
+
         xor eax, eax
         lodsb
         cmp al, 0
         je formatStringLoopEnd ; c string is terminated with \0
+
         cmp al, FORMAT_STRING_DELIM
-        ; je validFormatDelimeter
-        ;     call showErrorMessage
-        ; validFormatDelimeter:
+        je validFormatDelimeter
+            push rsi
+            call printSingleChar
+            pop rsi
+            jmp validFormatDelimIfEnd
+        validFormatDelimeter:
+            xor rax, rax
+            lodsb ; read another symbol
+            mov bl, al
 
-        xor eax, eax
-        lodsb
-        push rsi
-        call printNumberInDecimal
-        call printNewLine
-        pop rsi
+            mov rax, [rbp + 8 * rdi]
+            inc rdi
 
+            push rsi
+            ; switch on format type
+            cmp bl, FORMAT_STRING_DELIM
+            je percentCase
+            cmp bl, HEX_FORMAT_STRING
+            je hexademicalBaseCase
+            cmp bl, OCTAL_FORMAT_STRING
+            je octalBaseCase
+            cmp bl, DECIMAL_FORMAT_STRING
+            je decimalBaseCase
+            cmp bl, BOOL_FORMAT_STRING
+            je booleanTypeCase
+            cmp bl, CHAR_FORMAT_STRING
+            je charTypeCase
+            cmp bl, STRING_FORMAT_STRING
+            je stringTypeCase
+
+            percentCase:
+                dec rdi
+                mov al, '%'
+                call printSingleChar
+                jmp switchCaseEnd
+            hexademicalBaseCase:
+                mov rbx, 16
+                call printNumberInSomeBase
+                jmp switchCaseEnd
+            octalBaseCase:
+                mov rbx, 8
+                call printNumberInSomeBase
+                jmp switchCaseEnd
+            decimalBaseCase:
+                mov rbx, 10
+                call printNumberInSomeBase
+                jmp switchCaseEnd
+            booleanTypeCase:
+                call printBoolean
+                jmp switchCaseEnd
+            charTypeCase:
+                call printSingleChar
+                jmp switchCaseEnd
+            stringTypeCase:
+                call printString
+                jmp switchCaseEnd
+            switchCaseEnd:
+
+            pop rsi
+        validFormatDelimIfEnd:
 
         jmp formatStringCharsLoop
     formatStringLoopEnd:
 
-
-
-;     mov rdx, helloMessageLen
-;     mov rcx, rdx
-;     mov rsi, helloMessage
-;     mov rdi, tmpStringBuff
-;     rep movsb
-;     call myPrint
-;
-;     ;pop rsi
-;     mov rdx, rsi
-;     call printNumberInDecimal
-
-    ; mov rax, 0x01       ; syscall index of standard output function
-    ; mov rdi, 1          ; file descriptor for stdout
-    ; mov rsi, helloMessage
-    ; mov rdx, helloMessageLen
-    ; syscall
-
-    ;pop rbp
+    pop rbp
     ret
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-extern "C" void myPrintfFunction(const char* format, int a, const char* b);
+extern "C" void myPrintfFunction(const char* format, ...);
 
 int main() {
     printf("Hello world\n");
 
-    myPrintfFunction("%d%d%s", 29, "hello");
+    myPrintfFunction("num : %d, string : %s, bruh : %b\n", 10, "hello world!", false);
+    myPrintfFunction("char: %c, int: %d, bool: %b, oct: %o, hex: %x, d : %d, d : %d", 'a', 10, true, 18, 289, 19, 2829);
+    //myPrintfFunction("1) %d, 2) %d 3) %d 4) %d 5) %o 6) %o 7) %o 8) %b", 1, 2, 3, 4, 5, 6, 7, false);
 
     return 0;
 }
